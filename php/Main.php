f2779f06bb4bb14d4a54ae46adf0fab47a08ef52
Jonas <jona345e@edu.sde.dk>
2019-04-29 11:23:37 +0200

    now with php

---
+++ php/Main.php
@@ -0,0 +1,23 @@
+<?php
+require 'rb.php';
+R::setup('mysql:host=localhost;dbname=entest', 'root', 'Rootlog');
+
+$opts = getopt('',['add:','list']);
+
+if ( isset( $opts['add'] ) ) {
+    $w = R::dispense( 'whisky' );
+    $w->name = $opts['add'];
+    $id = R::store( $w );
+    die( "OK.\n" );
+}
+if ( isset( $opts['list'] ) ) {
+    $bottles = R::find( 'whisky' );
+    if ( !count( $bottles ) )
+        die( "The cellar is empty!\n" );
+    foreach( $bottles as $b ) {
+        echo "* #{$b->id}: {$b->name}\n";
+    }
+    exit;
+}
+
+?>
